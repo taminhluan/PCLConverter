4a8b98bb254f37c41091bd0c02b864b4eb79061e
luantm <ngoalongtb001@gmail.com>
2021-11-12 15:48:55 +0900

    prepare menu, add README.txt

--- include/pcl_converter.h
+++ include/pcl_converter.h
@@ -2,14 +2,14 @@
 
 void print_usage();
 
-void info();
+void info(std::string input_path);
 
 void print_points(unsigned int number_of_points);
 
-void convert(std::string output_filepath);
+void convert(std::string input_path, std::string output_path);
 
-void split(unsigned int chunk_size, std::string extension);
+void split(std::string input_path, unsigned int chunk_size, std::string extension);
 
-void make_octree(unsigned int chunk_size, std::string extension);
+void make_octree(std::string input_path, unsigned int chunk_size, std::string extension);
 
-void make_LOD(unsigned int chunk_size, std::string extension);
+void make_LOD(std::string input_path, unsigned int chunk_size, std::string extension);
--- src/pcl_converter.cpp
+++ src/pcl_converter.cpp
@@ -4,61 +4,107 @@
 #include <pcl_converter.h>
 
 void print_usage() {
-    std::cout << "pcl_converter -h" << ": see how to use" << "\n";
-    std::cout << "pcl_converter -info *.las/.laz" 
+    std::cout << "PCLConverter -h" << ": see how to use" << "\n";
+    std::cout << "PCLConverter -info *.las/.laz" 
                 <<": print file info"
                 << "\n";
 
-    std::cout << "pcl_converter -convert in.las/.laz/.txt/.pts output.las" 
+    std::cout << "PCLConverter -convert in.las/.laz/.txt/.pts output.las" 
                 <<": convert to another format"
                 << "\n";
                 
-    std::cout << "pcl_converter -split in.las/.laz/.txt/.pts output.las chuck_size" 
+    std::cout << "PCLConverter -split in.las/.laz/.txt/.pts output.las chuck_size" 
                 <<": split to many chunks"
                 << "\n";
 
-    std::cout << "pcl_converter -octree in.las/.laz/.txt/.pts output.las chuck_size" 
+    std::cout << "PCLConverter -octree in.las/.laz/.txt/.pts output.las chuck_size" 
                 <<": make octree"
                 << "\n";
     
-    std::cout << "pcl_converter -lod in.las/.laz/.txt/.pts output.las chuck_size" 
+    std::cout << "PCLConverter -lod in.las/.laz/.txt/.pts output.las chuck_size" 
                 <<": make LOD"
                 << "\n";
 }
 
-void info() {
-    std::cout << "under constrution";
+void info(std::string input_path) {
+    std::cout << "GET INFO";
 }
 
 void print_points(unsigned int number_of_points) {
-    std::cout << "under constrution";
+    std::cout << "PRINT " << number_of_points << " first POINTS";
 }
 
-void convert(std::string output_filepath) {
-    std::cout << "under constrution";
+void convert(std::string input_path, std::string output_path) {
+    std::cout << "CONVERT\n";
+    std::cout << input_path <<"\n";
+    std::cout << output_path << "\n";
 }
 
-void split(unsigned int chunk_size, std::string extension) {
-    std::cout << "under constrution";
+void split(std::string input_path, unsigned int chunk_size, std::string extension) {
+    std::cout << "SPLIT\n";
+    std::cout << input_path <<"\n";
+    std::cout << chunk_size << "\n";
+    std::cout << extension << "\n";
 }
 
-void make_octree(unsigned int chunk_size, std::string extension) {
-    std::cout << "under constrution";
+void make_octree(std::string input_path, unsigned int chunk_size, std::string extension) {
+    std::cout << "MAKE OCTREE\n";
+    std::cout << input_path <<"\n";
+    std::cout << chunk_size << "\n";
+    std::cout << extension << "\n";
 }
 
-void make_LOD(unsigned int chunk_size, std::string extension) {
-    std::cout << "under constrution";
+void make_LOD(std::string input_path, unsigned int chunk_size, std::string extension) {
+    std::cout << "MAKE LOD\n";
+    std::cout << input_path <<"\n";
+    std::cout << chunk_size << "\n";
+    std::cout << extension << "\n";
 }
 
-int main() {
-    std::cout << "PCL Converter";
+int main(int argc, char *argv[]) {
+    std::cout << "PCL Converter" << " ";
+    std::cout << argc << "\n";
+    std::cout << argv[1] << "\n";
+    // std::cout << "argc " << argc <<  argv[0] ;
+    if (argc > 1) {
+
+        // "PCLConverter -h"
+        if ( strcmp( argv[1], "-h") == 0 ) {
+            std::cout << "in here";
+            print_usage();
+        } else if ( strcmp( argv[1], "-info") == 0 &&
+                     argc > 1 ) { //"PCLConverter -info *.las/.laz"
+            std::string input_path(argv[2]);
+            info(input_path);
+        } else if ( strcmp( argv[1], "-convert") == 0 && 
+                    argc > 2 ) { //"PCLConverter -info input.las output.laz" 
+            std::string input_path(argv[2]);
+            std::string output_path(argv[3]);
+            convert(input_path, output_path);
+        } else if ( strcmp( argv[1], "-split") == 0 &&
+                    argc > 2 ) { //"PCLConverter -info input.las output.laz" 
+            std::string input_path(argv[2]);
+            split(input_path, 1000000, ".txt");
+        } else if ( strcmp( argv[1], "-octree") == 0 && 
+                    argc > 2 ) { //"PCLConverter -info input.las output.laz" 
+            std::string input_path(argv[2]);
+            make_octree(input_path, 1000, ".txt");
+        } else if ( strcmp( argv[1], "-lod") == 0 &&
+                    argc > 2 ) { //"PCLConverter -info input.las output.laz" 
+            std::string input_path(argv[2]);
+            make_octree(input_path, 1000, ".txt");
+        } else {
+            print_usage();
+        }
+    } else {
+        print_usage();
+    }
 
     if (laszip_load_dll())
     {
         std::cout << "DLL ERROR: loading LASzip DLL\n";
         std::cout << "You should copy laszip.dll to binary folder (which contain pcl_converter.exe)";
     }
-
 
     return 0;
 }
